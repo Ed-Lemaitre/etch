023412fbf6f0714b6a14bbae49c1b96ae6012f92
Ed-Lemaitre <97646941+Ed-Lemaitre@users.noreply.github.com>
2022-11-18 19:09:41 -0300

    first transtion achieved!!!

--- scripts.js
+++ scripts.js
@@ -12,3 +12,27 @@ for (i = 1; i <= 16; i++) {
     divOutter = divOutter.cloneNode(true);
 }
 
+const boxes = document.querySelectorAll('div.grid div div');
+
+boxes.forEach((box) => {
+    box.addEventListener('mouseover', addTransition);
+});
+
+boxes.forEach((box) => {
+    box.addEventListener('transitionend', removeTransition);
+});
+
+// const classBoxes = document.querySelectorAll('div.transition');
+
+// classBoxes.forEach((box) => {
+//     console.log("hola")
+//     box.addEventListener('transitionend', removeTransition);
+// });
+
+function addTransition(e) {
+    this.classList.add('changeColor');
+}
+
+function removeTransition(e) {
+    this.classList.remove('changeColor');
+}
