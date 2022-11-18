fa092dd58585fb6260cd93e28069920261bb85a3
Ed-Lemaitre <97646941+Ed-Lemaitre@users.noreply.github.com>
2022-11-18 16:55:14 -0300

    NOW finished flex grid

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ const mainDiv = document.querySelector('.grid');
 var divOutter = document.createElement('div');
 var divInner = document.createElement('div');
 
-for (i = 1; i <= 4; i++) {
+for (i = 1; i <= 16; i++) {
     divOutter.appendChild(divInner);
     divInner = divInner.cloneNode(true);
 }
 
-for (i = 1; i <= 4; i++) {
+for (i = 1; i <= 16; i++) {
     mainDiv.appendChild(divOutter);
     divOutter = divOutter.cloneNode(true);
 }
